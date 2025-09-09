2f89c284110cf7385b654a038a9fbd8bd11c99c5
David Kendall <me@davidskendall.co.uk>
2025-09-09 07:35:53 +0100

    feat: Added new template for secondary contacts who cannot login

---
+++ layouts/secondaryContactLoginAttempt/model.ts
@@ -0,0 +1,14 @@
+/**
+ * Specify required object
+ *
+ * @examples require(".").sampleData
+ */
+export interface IModel {
+  firstName?: string
+}
+
+export const sampleData: IModel[] = [
+  {
+    firstName: 'firstName',
+  },
+]
