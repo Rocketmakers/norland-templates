db5d6686da98777698fc389b6025654d47bc9f78
David Kendall <me@davidskendall.co.uk>
2026-05-07 09:07:22 +0100

    chore: Updated template based on new structure

--- layouts/anonymisationScheduleReport/model.ts
+++ layouts/anonymisationScheduleReport/model.ts
@@ -4,11 +4,15 @@
  * @examples require(".").sampleData
  */
 export interface IModel {
+  numberOfClients: number;
   scheduledTimestamp: string;
+  returnUrl: string;
 }
 
 export const sampleData: IModel[] = [
   {
+    numberOfClients: 5,
     scheduledTimestamp: "2026-05-06T00:00:00Z",
+    returnUrl: "https://example.com/clients"
   },
 ]
